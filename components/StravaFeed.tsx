4909942ce4389b95c0dc05f7f697eed7d245c02c
Kyle Czajkowski <38336076+Kyle-Ski@users.noreply.github.com>
2022-11-16 13:12:03 -0700

    adding strava runs.. (#9)

---
+++ components/StravaFeed.tsx
@@ -0,0 +1,20 @@
+import { STRAVA_TITLE } from '../utils/constants'
+import styles from '../styles/Strava.module.css'
+
+const StravaFeed = () => {
+  return (
+    <div className={styles.iframeWrapper}>
+      <h2 id={STRAVA_TITLE}>Checkout my recent runs on Strava:</h2>
+      <iframe
+        className={styles.iframe}
+        height="454"
+        width="300"
+        frameBorder="0"
+        scrolling="no"
+        src="https://www.strava.com/athletes/46337708/latest-rides/cfb328f35bb663d45111215b56632d2f6350526e"
+      ></iframe>
+    </div>
+  )
+}
+
+export default StravaFeed
