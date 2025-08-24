c796271188daf780f6426be896367a074000bb1b
Kyle-Ski <skiroyjenkins@gmail.com>
2025-08-23 23:06:03 -0700

    updating google tag manager?

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,15 +15,17 @@ export const metadata = {
   openGraph: {
     title: 'Kyle Czajkowski - Developer & Adventurer',
     description: 'Software engineer specializing in React, TypeScript, and Node.js. Sharing insights on web development, outdoor adventures, and gear reviews.',
-    images: ['/og-image.jpg'],
+    images: ['/Me.jpg'],
     url: 'https://kyle.czajkowski.tech'
   },
   twitter: {
     card: 'summary_large_image',
     creator: '@SkiRoyJenkins'
+  },
+  alternates: {
+    canonical: 'https://kyle.czajkowski.tech'
   }
 };
-
 
 export default function RootLayout({
   children,
@@ -33,35 +35,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Analytics */}
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-            ga('create', 'UA-XXXXX-Y', 'auto');
-            ga('send', 'pageview');`,
-          }}
-        />
-
-        {/* Google Tag Manager */}
+        {/* Google Tag Manager - As high in head as possible */}
         <Script
           id="google-tag-manager"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MJR2GX3');`,
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MJR2GX3');`,
           }}
         />
       </head>
       <body>
-        {/* Google Tag Manager noscript */}
+        {/* Google Tag Manager noscript - Immediately after opening body tag */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-MJR2GX3"
